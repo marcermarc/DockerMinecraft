59cd669ea484af384b33c068d3c35cf39ad79a0c
marcermarc <marcermarc@gmail.com>
2017-04-16 16:04:26 +0200

    Updated

--- StartProgram/Start.java
+++ StartProgram/Start.java
@@ -1,10 +1,10 @@
 import java.io.File;
-import java.io.IOException;
 import java.io.OutputStream;
 
 public class Start {
     private static Process process;
     private static OutputStream output;
+	private static boolean restart = true;
 
     public static void main(String args[]) {
         if (args.length != 2) {
@@ -12,28 +12,29 @@ public class Start {
             return;
         }
 
-        Runtime.getRuntime().addShutdownHook(new Thread() {
+        Runtime.getRuntime().addShutdownHook(new Thread() { // get the signal to stop the process
             @Override
             public void run() {
                 try {
-                    output.write("stop".getBytes());
-                } catch (IOException ex) {
+					restart = false; // stop the endless loop for restarting the server
+                    output.write(new byte[] {(byte)0x73,(byte)0x74,(byte)0x6f,(byte)0x70,(byte)0x0d,(byte)0x0a}); // The byte-array represents "stop" and a return char
+					output.flush(); // write the buffer to the process
+					process.waitFor(); // wait for the server-stop
+					Runtime.getRuntime().halt(0); // retrun 0 so the process end is succsessfull
+                } catch (Exception ex) {
                     System.out.println("Stop Failed: " + ex.getMessage());
                 }
             }
         });
 
-        try {
-            process = Runtime.getRuntime().exec(args[0], null, new File(args[1]));
-            output = process.getOutputStream();
-        } catch (Exception ex) {
-            System.out.println("Start Failed: " + ex.getMessage());
-        }
-
-        try {
-            process.waitFor();
-        } catch (InterruptedException ex) {
-            System.out.println("Wait Failed: " + ex.getMessage());
-        }
+		while (restart) {
+			try {
+				process = Runtime.getRuntime().exec(args[0], null, new File(args[1])); // init the process
+				output = process.getOutputStream(); // get the output-stream to write commands to the process
+				process.waitFor(); // wait for server-stop
+			} catch (Exception ex) {
+				System.out.println("Execution Failed: " + ex.getMessage());
+			}
+		}
     }
 }
