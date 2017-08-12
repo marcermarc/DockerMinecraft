c2464887aa53aead03c774ecae2c3db4357eb304
marcermarc <marcermarc@gmail.com>
2017-08-12 18:02:32 +0200

    Update Features of Start.java

--- StartProgram/Start.java
+++ StartProgram/Start.java
@@ -1,37 +1,228 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 public class Start {
+
+    //<editor-fold desc="Parameter-Strings">
+    private static final String COMMAND = "command";
+    private static final String WORKDIR = "workdir";
+    private static final String WORLD_PATH = "worldpath";
+    private static final String USE_RAMDISK = "useramdisk";
+    private static final String RAMDISK_BACKUP_INTERVAL = "ramdiskbackupinterval"; // In minutes
+    private static final String MAX_COPY_TIME = "maxcopytime"; // In seconds
+    private static final String RESTART_INTERVAL = "restartinterval"; // In minutes
+    private static final String MAX_SHUTDOWN_TIME = "maxshutdowntime"; // In seconds
+    //</editor-fold>
+
+    private static final String RAMDISK_WORLD_PATH = "/dev/shm/world";
+    private static final Map<String, String> parameters = new HashMap<>();
+    private static File workdirFile;
+    private static Timer timer;
     private static Process process;
     private static OutputStream output;
-    private static boolean restart = true;
+    private static boolean restart = true; // while this is true minecraft is restarted
+
+    static {
+        // Default values:
+        parameters.put(WORLD_PATH, "world"); // the relativ path from workdir is used
+        parameters.put(RAMDISK_BACKUP_INTERVAL, "60");
+        parameters.put(MAX_COPY_TIME, "30");
+        parameters.put(MAX_SHUTDOWN_TIME, "30");
+    }
 
     public static void main(String args[]) {
-        if (args.length != 2) {
-            System.out.println("Two Arguments needed, first command, second path");
-            return;
+
+        // load all parameters, environment overwrites default, args overwrites environment
+        getEnvironment();
+        parseArgs(args);
+        if (!testParameters()) {
+            System.out.println("Minimum parameters not filled");
+            Runtime.getRuntime().halt(-1);
         }
+        workdirFile = new File(parameters.get(WORKDIR));
 
-        // get the signal to stop the process
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+        initRamdisk();
+        initRestart();
+        initShutdownHook();
+        initInputHandleThread();
+
+        runMinecraft();
+    }
+
+    private static void runMinecraft() {
+        int errorCounter = 0;
+
+        while (restart && errorCounter < 10) { // if there are to many errors the container should stop
             try {
-                restart = false; // stop the endless loop for restarting the server
+                ProcessBuilder pb = new ProcessBuilder(parameters.get(COMMAND).split(" ")) // split the command because every parameter has to be an own string
+                        .directory(workdirFile) // set the startup directory (external of the container)
+                        .inheritIO() // share the console with this process
+                        .redirectInput(ProcessBuilder.Redirect.PIPE); // return the input stream back to this program so the stop command can be send
 
-                if (output != null) {
-                    output.write(new byte[]{(byte) 0x73, (byte) 0x74, (byte) 0x6f, (byte) 0x70, (byte) 0x0d, (byte) 0x0a}); // The byte-array represents "stop" and a return char
-                    output.flush(); // write the buffer to the process
+                process = pb.start(); // init the process
+                output = process.getOutputStream(); // get the output-stream to write commands to the process
+                process.waitFor(); // wait for server-stop
+                output = null;
+
+                if (restart) {
+                    backupRamdisk(); // because minecraft is stopped a backup is now usefull
                 }
 
-                process.waitFor(); // wait for the server-stop
-                Runtime.getRuntime().halt(0); // return 0 so the process end is success
             } catch (Exception ex) {
-                System.out.println("Stop Failed: " + ex.getMessage());
+                System.out.println("Execution Failed: " + ex.getMessage());
+                errorCounter++;
             }
-        }));
+        }
+    }
 
-        // this thread handle the input and hands the input off to the process
+    //<editor-fold desc="Init">
+    private static void parseArgs(String args[]) {
+        for (int i = 0; i < args.length - 1; i++) {
+            if (args[i] != null && args[i].startsWith("-")) {
+                parameters.put(args[i].substring(1).toLowerCase(), args[i + 1] == null || args[i + 1].startsWith("-") ? null : args[i + 1]);
+                i++;
+            }
+        }
+    }
+
+    private static void getEnvironment() {
+        for (Map.Entry<String, String> ent : System.getenv().entrySet()) {
+            parameters.put(ent.getKey().toLowerCase(), ent.getValue());
+        }
+    }
+
+    private static boolean testParameters() {
+        return parameters.containsKey(COMMAND) && parameters.get(COMMAND) != null && !parameters.get(COMMAND).isEmpty() &&
+                parameters.containsKey(WORKDIR) && parameters.get(WORKDIR) != null && !parameters.get(WORKDIR).isEmpty() &&
+
+                // test if parameters are numbers
+                isNuberic(parameters.get(RAMDISK_BACKUP_INTERVAL)) &&
+                isNuberic(parameters.get(MAX_COPY_TIME)) &&
+                isNuberic(parameters.get(MAX_SHUTDOWN_TIME)) &&
+                (!parameters.containsKey(RESTART_INTERVAL) || isNuberic(parameters.get(RESTART_INTERVAL))) // this parameter is not needed
+                ;
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Stop">
+    private static void stopCommand() throws IOException {
+        if (output != null) {
+            output.write(new byte[]{(byte) 0x73, (byte) 0x74, (byte) 0x6f, (byte) 0x70, (byte) 0x0d, (byte) 0x0a}); // The byte-array represents "stop" and a return char
+            output.flush(); // write the buffer to the process
+        }
+    }
+
+    private static void stop(boolean haltThisIfFailed) {
+        try {
+            stopCommand();
+            process.waitFor(Long.parseLong(parameters.get(MAX_SHUTDOWN_TIME)), TimeUnit.SECONDS);
+        } catch (Exception ex) {
+            System.out.println("Minecraft Shutdown Failed. (Maybe max shutdown time is to short)");
+
+            if (haltThisIfFailed) {
+                System.out.println("To stop minecraft the docker-container is shutting down.");
+                Runtime.getRuntime().exit(-1);
+            }
+        }
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Ram Disk">
+    private static void initRamdisk() {
+        if (parameters.containsKey(USE_RAMDISK)) {
+            try {
+                copy(parameters.get(WORLD_PATH), RAMDISK_WORLD_PATH, workdirFile);
+            } catch (Exception ex) {
+                System.out.println("Ramdisk-Creation: Copy Failed: " + ex.getMessage());
+                Runtime.getRuntime().halt(-1);
+            }
+
+            if (Long.parseLong(parameters.get(RAMDISK_BACKUP_INTERVAL)) > 0) { // if the backup is not manually switched off
+                if (timer == null) {
+                    timer = new Timer();
+                }
+
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        backupRamdisk();
+                    }
+                }, Long.parseLong(parameters.get(RAMDISK_BACKUP_INTERVAL)) * 60 * 1000); // delay in milliseconds: RAMDISK_BACKUP_INTERVAL * 60 (seconds per minute) * 1000 (milliseconds per second)
+            }
+        }
+    }
+
+    private static void backupRamdisk() {
+        if (parameters.containsKey(USE_RAMDISK)) {
+            try {
+                copy(RAMDISK_WORLD_PATH, parameters.get(WORLD_PATH), workdirFile);
+            } catch (Exception ex) {
+                System.out.println("Ramdisk-Backup: Copy Failed: " + ex.getMessage());
+            }
+        }
+    }
+
+    private static void finilizeRamdisk() {
+        if (parameters.containsKey(USE_RAMDISK)) {
+            try {
+                copy(RAMDISK_WORLD_PATH, parameters.get(WORLD_PATH), workdirFile);
+            } catch (Exception ex) {
+                System.out.println("Ramdisk-Finalize: Copy Failed (DATA IS LOST): " + ex.getMessage());
+            }
+        }
+    }
+
+    private static synchronized void copy(String from, String to, File workdir) throws IOException, InterruptedException { // synchronized to avoid doing two copies at one time
+        new ProcessBuilder("mkdir", "-p", to) // create the path
+                .directory(workdir)
+                .start()
+                .waitFor(1, TimeUnit.SECONDS); // should not last longer then one second
+
+        new ProcessBuilder("cp", "-r", "-u", from + "/.", to) // copy the files
+                .directory(workdir)
+                .start()
+                .waitFor(Long.parseLong(parameters.get(MAX_COPY_TIME)), TimeUnit.MINUTES); // should not last longer then the given minutes
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Restart">
+    private static void initRestart() {
+        if (parameters.containsKey(RESTART_INTERVAL)) {
+            if (timer == null) {
+                timer = new Timer();
+            }
+
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    stop(true);
+                }
+            }, Long.parseLong(parameters.get(RESTART_INTERVAL)) * 60 * 1000); // delay in milliseconds: RAMDISK_BACKUP_INTERVAL * 60 (seconds per minute) * 1000 (milliseconds per second)
+        }
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Shutdown hook">
+    private static void initShutdownHook() {
+        // get the signal to stop the process
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            restart = false; // stop the endless loop for restarting the server
+
+            stop(false); // stop minecraft
+
+            finilizeRamdisk(); // to copy the data back from the Ramdisk
+
+            Runtime.getRuntime().halt(0); // return 0 so the process end is success
+        }));
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Input-handle thread">
+    private static void initInputHandleThread() {
         new Thread(() -> {
             BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
             while (true) {
@@ -47,25 +238,26 @@ public class Start {
                 }
             }
         }).start();
+    }
+    //</editor-fold>
 
-        while (restart) {
-            try {
-                String command = args[0]
-                        .replaceAll("&HEAP&", System.getenv().get("HEAP")) // replace the HEAP
-                        .replaceAll("&PARAMS&", System.getenv().get("PARAMS")); //replace the PARAMS
+    //<editor-fold desc="Util">
+    private static boolean isNuberic(String str) {
+        if (str == null) {
+            return false;
+        }
+        int length = str.length();
+        if (length == 0) {
+            return false;
+        }
 
-                ProcessBuilder pb = new ProcessBuilder(command.split(" ")) // split the command because every parameter has to be an own string
-                        .directory(new File(args[1])) // set the startup directory (external of the container)
-                        .inheritIO() // share the console with this process
-                        .redirectInput(ProcessBuilder.Redirect.PIPE); // return the input stream back to this program so the stop command can be send
-
-                process = pb.start(); // init the process
-                output = process.getOutputStream(); // get the output-stream to write commands to the process
-                process.waitFor(); // wait for server-stop
-                output = null;
-            } catch (Exception ex) {
-                System.out.println("Execution Failed: " + ex.getMessage());
+        for (int i = 0; i < length; i++) {
+            char c = str.charAt(i);
+            if (c < '0' || c > '9') {
+                return false;
             }
         }
+        return true;
     }
+    //</editor-fold>
 }
